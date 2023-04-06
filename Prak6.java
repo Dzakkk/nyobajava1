a76af4a2755c63dd737de85003b206dbddc721a9
Dzakwan Falahudin Pratama <113731304+Dzakkk@users.noreply.github.com>
2023-04-06 11:27:14 +0700

    Create Prak6.java

---
+++ Prak6.java
@@ -0,0 +1,9 @@
+public class Prak6 {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        Buku buku1 = new Buku("Cara Menjadi jenius no root",209);
+
+        System.out.println(buku1.getJudul());
+        System.out.println(buku1.getHalaman());
+    }
+}
